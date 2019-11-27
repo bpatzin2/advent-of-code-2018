2ce1ee0a55ebd12b6223991889ea5c99c1ce95ce
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2019-11-27 13:48:35 -0600

    include real input in test

--- day1/chronal-calibration/spec/chronal_calibration/core_spec.clj
+++ day1/chronal-calibration/spec/chronal_calibration/core_spec.clj
@@ -3,26 +3,20 @@
             [chronal-calibration.core :refer :all]))
 
 (describe "sum_comma_separated_numbers"
-
-          (it "works"
+          (it "works for test input"
               (should= 3 (sum_comma_separated_numbers "+1, +1, +1"))
               (should= 0 (sum_comma_separated_numbers "+1, +1, -2"))
-              (should= -6 (sum_comma_separated_numbers "-1, -2, -3"))))
-
-(describe "rolling_sum_comma_separated_numbers"
-
-          (it "works"
-              (should= [0, 1, 2, 3] (rolling_sum_comma_separated_numbers "+1, +1, +1"))
-              (should= [0, 1, 2, 0] (rolling_sum_comma_separated_numbers "+1, +1, -2"))
-              (should= [0, -1, -3, -6] (rolling_sum_comma_separated_numbers "-1, -2, -3"))))
+              (should= -6 (sum_comma_separated_numbers "-1, -2, -3")))
+          (it "works for real input"
+              (should= 569 (sum_comma_separated_numbers (gen-csv)))))
 
 (describe "first-dup-in-rolling-sum"
-
-          (it "works"
+          (it "works for test input"
               (should= 0 (first-dup-in-rolling-sum "+1, -1"))
               (should= 10 (first-dup-in-rolling-sum "+3, +3, +4, -2, -4"))
               (should= 5 (first-dup-in-rolling-sum "-6, +3, +8, +5, -6"))
-              (should= 14 (first-dup-in-rolling-sum "+7, +7, -2, -7, -4"))))
-
+              (should= 14 (first-dup-in-rolling-sum "+7, +7, -2, -7, -4")))
+          (it "works for real input"
+              (should= 77666 (first-dup-in-rolling-sum (gen-csv)))))
 
 (run-specs)
