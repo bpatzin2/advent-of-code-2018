e16768e922077336c7b23206ff7e280593593429
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2019-11-26 21:16:16 -0600

    tests pass but too slow

--- day1/chronal-calibration/spec/chronal_calibration/core_spec.clj
+++ day1/chronal-calibration/spec/chronal_calibration/core_spec.clj
@@ -9,4 +9,20 @@
               (should= 0 (sum_comma_separated_numbers "+1, +1, -2"))
               (should= -6 (sum_comma_separated_numbers "-1, -2, -3"))))
 
+(describe "rolling_sum_comma_separated_numbers"
+
+          (it "works"
+              (should= [0, 1, 2, 3] (rolling_sum_comma_separated_numbers "+1, +1, +1"))
+              (should= [0, 1, 2, 0] (rolling_sum_comma_separated_numbers "+1, +1, -2"))
+              (should= [0, -1, -3, -6] (rolling_sum_comma_separated_numbers "-1, -2, -3"))))
+
+(describe "first-dup-in-rolling-sum"
+
+          (it "works"
+              (should= 0 (first-dup-in-rolling-sum "+1, -1"))
+              (should= 10 (first-dup-in-rolling-sum "+3, +3, +4, -2, -4"))
+              (should= 5 (first-dup-in-rolling-sum "-6, +3, +8, +5, -6"))
+              (should= 14 (first-dup-in-rolling-sum "+7, +7, -2, -7, -4"))))
+
+
 (run-specs)
--- day1/chronal-calibration/src/chronal_calibration/core.clj
+++ day1/chronal-calibration/src/chronal_calibration/core.clj
@@ -14,5 +14,34 @@
 (defn sum_comma_separated_numbers [numbers_string]
   (reduce + (string-to-int-vector numbers_string)))
 
+(defn rolling-sum [vec, val] 
+  (conj vec (+ (last vec) val)))
+
+(defn reduced-dup-val-or-append [vec, val]
+  (if (contains? vec val)
+    (reduced val) 
+    (conj vec val)))
+
+(defn rolling_sum_comma_separated_numbers [numbers_string]
+  (reduce rolling-sum [0] (string-to-int-vector numbers_string)))
+
+(defn ever-growing-cycles [numbers_string]
+  (map #(str/join ", " %1) (reductions conj [numbers_string] (repeat numbers_string))))
+
+(defn ever-growing-rolling-sums [numbers_string]
+  (map rolling_sum_comma_separated_numbers (ever-growing-cycles numbers_string)))
+
+(defn reduce-to-val-or-nil [vec]
+  (reduce reduced-dup-val-or-append #{} vec))
+
+(defn first-repeated-sum-or-rolling-sums [numbers_string]
+  (map reduce-to-val-or-nil (ever-growing-rolling-sums numbers_string)))
+
+(defn val-or-nil [_, val]
+  (if (coll? val) (constantly nil) (reduced val)))
+
+(defn first-dup-in-rolling-sum [numbers_string]
+  (reduce val-or-nil [] (first-repeated-sum-or-rolling-sums numbers_string)))
+
 (defn -main []
   (sum_comma_separated_numbers "-1, 1, 0"))
