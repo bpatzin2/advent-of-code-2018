fb884a4e9765b6fcad9776e4f24e337613022f95
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2022-09-24 21:08:27 +0200

    day1 pt2 works

---
+++ day1pt2.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs';
+
+const changes = getChangesFromFile();
+const result = firstDuplicateFrequency(changes)  
+console.log(result)  
+
+function firstDuplicateFrequency(changes: number[]) {
+  let frequency = 0
+  const frequencies = new Set<number>([frequency])
+  let i = 0
+  while(true){
+    const change = changes[i]
+    frequency += change
+    if (frequencies.has(frequency)){
+        return frequency
+    }
+    frequencies.add(frequency)
+
+    i++
+    if(i == changes.length){
+      i = 0
+    }
+  }
+}
+
+function getChangesFromFile() {
+  const allFileContents = fs.readFileSync('day1-input.txt', 'utf-8');
+  const numbersStrs = allFileContents.trim().split(/\r?\n/);
+  return numbersStrs
+    .map(x => x.replace("+", ""))
+    .map(x => parseInt(x));
+}
+
