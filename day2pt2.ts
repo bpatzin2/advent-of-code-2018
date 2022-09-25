2dd8b2ffa961252c87f20d86d979b7fc68ea3e3e
Brian Patzin <bpatzin@Brians-MacBook-Air.local>
2022-09-25 20:54:20 +0200

    day2 pt2 works

---
+++ day2pt2.ts
@@ -0,0 +1,32 @@
+import * as fs from 'fs';
+
+const fileContent = fs.readFileSync('day2-input.txt', 'utf-8');
+const fileLines = fileContent.trim().split(/\r?\n/)
+const result = findDifferentBy1Commonality(fileLines)
+console.log(result)
+
+function findDifferentBy1Commonality(strs: string[]): string|null {
+  const lineLength = strs[0].length
+  let i = 0
+  while(i < lineLength){
+    const line = strs[i]
+    const m = new Map<string, string>()
+    let j = 0
+    while(j < strs.length){
+      const line = strs[j]
+      const edittedLine = removeCharAtIndex(line, i)
+      const other = m.get(edittedLine)
+      if (other != null){
+        return edittedLine
+      } 
+      m.set(edittedLine, line)
+      j++
+    }
+    i++
+  }
+  return null
+}
+
+function removeCharAtIndex(str: string, i: number): string {
+  return str.slice(0, i) + str.slice(i+1)
+}
